ffb0a52479190ee48d5289d62fc95833bdbf9389
xmegan10 <xmegan10@gmail.com>
2025-10-29 09:50:18 -0400

    add output of build

--- Assignment_10_Building_R_Package/Assignment_10.R
+++ Assignment_10_Building_R_Package/Assignment_10.R
@@ -7,3 +7,16 @@ create("Friedman")
 #Verify and Build
 check("Friedman")
 build("Friedman")
+
+#Output of build:
+#── R CMD build ─────────────────────────────────────────────────
+#✔  checking for file ‘/Users/meganxiao/Desktop/USF/Fall 2025/LIS4370 R Programming/R Programming Assignments/Friedman/DESCRIPTION’ ...
+#─  preparing ‘Cleaner’:
+#  ✔  checking DESCRIPTION meta-information ...
+#─  checking for LF line-endings in source and make files and shell scripts
+#─  checking for empty or unneeded directories
+#Removed empty directory ‘Cleaner/R’
+#Removed empty directory ‘Cleaner/man’
+#─  building ‘Cleaner_0.0.0.9000.tar.gz’
+
+#[1] "/Users/meganxiao/Desktop/USF/Fall 2025/LIS4370 R Programming/R Programming Assignments/Cleaner_0.0.0.9000.tar.gz"
